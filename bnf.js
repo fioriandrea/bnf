cc7b2ed7d133c37159c8c0584e3e27b1f989a4ec
andrea <andrea.fiori.1998@gmail.com>
2019-12-05 19:40:12 +0000

    improve design

--- bnf.js
+++ bnf.js
@@ -1,31 +1,37 @@
+//Rules and non-terminals go always between #
+//(e.g "#rule#": "#rule# terminal",)
+
 const rules = {
-  "assign": "#id# = #expr#",
-  "expr": ["#id# + #expr#", "#id# * #expr#", "#id# / #expr#", "#id# - #expr#", "#id#", "(#expr#)"],
-  "id": ["a", "b", "c", "d", "e", "f", "g"],
+  "#phrase#": ["#comp-noun# #is-stat#", "#phrase# #that-clause# #is-stat#"],
+  "#comp-noun#": "#art# #noun#",
+  "#is-stat#": "#be# #comp-noun#",
+  "#be#": ["is", "is not", "isn't"],
+  "#that-clause#": ["that", ", which"],
+  "#art#": "a",
+  "#noun#": ["cat", "dog", "monkey", "mouse", "computer", "human"],
+
+  /*"#assign#": "#id# = #expr#",
+  "#expr#": ["#id# + #expr#", "#id# * #expr#", "#id# / #expr#", "#id# - #expr#", "#id#", "(#expr#)"],
+  "#id#": ["a", "b", "c", "d", "e", "f", "g"],*/
 };
 
 const nonTerminalRegex = /#([^#]*)#/g;
 
-const maxCalls = 10000;
-
 const randomElem = array => array[Math.floor(Math.random()*array.length)];
 
-const randomPhrase = (axiom, n = 0) => {
-  if(n > maxCalls) return axiom.replace(/#.*#/g, "");
-
-  const callback = match => {
-    const nonTerminal = match.replace(/#/g, "");
-    return randomPhrase(nonTerminal, n + 1);
+const randomPhrase = axiom => {
+  const callback = nonTerminal => {
+    return randomPhrase(nonTerminal);
   };
 
-  if(!rules[axiom]) {
+  if(!rules[axiom]) { // phrase with nonterminal in or terminal phrase
     return axiom.replace(nonTerminalRegex, callback);
   }
 
   const rule = rules[axiom];
   if(Array.isArray(rule)) {
     const subRule = randomElem(rule);
-    return randomPhrase(subRule, n + 1);
+    return subRule.replace(nonTerminalRegex, callback);
   }
   else {
     return rule.replace(nonTerminalRegex, callback);
